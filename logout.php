9664f712c5365c836b277f4b138ad5054316a35e
gotedkid195 <40803862+gotedkid195@users.noreply.github.com>
2018-08-25 10:00:15 +0700

    init logout

--- logout.php
+++ logout.php
@@ -1,14 +1,8 @@
 <?php 
  session_start();
-if(isSet($_SESSION['username']))
-{
-	unset($_SESSION['username']);
-	unset($_SESSION['id']);}
-if(isSet($_COOKIE['cookie_name']))
-{
-	setcookie('cookie_name', '', time() - (3600 * 24 * 30));
-	echo $_COOKIE['cookie_name'];
-}
-	header('Location: index.php'); 
-	exit;
-?>
+ unset($_SESSION['id']);
+ unset($_SESSION['username']);
+setcookie('cookie_name', '',time()-2592000,'/');
+
+ header('Location: ../index.php'); 
+ ?>
